67676e67a4351f510644b829a1c51ba24ddebcd8
Vlad <vladfilimontsev@yandex.ru>
2025-06-16 19:39:07 +0300

    Add logger

--- src/main/java/com/edme/eurekadiscoveryserver/EurekaDiscoveryServerApplication.java
+++ src/main/java/com/edme/eurekadiscoveryserver/EurekaDiscoveryServerApplication.java
@@ -1,8 +1,12 @@
 package com.edme.eurekadiscoveryserver;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,6 +16,13 @@ public class EurekaDiscoveryServerApplication {
 
         //SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
         SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder().maximumSize(1000).weakKeys().recordStats());
+        return cacheManager;
     }
 
 }
